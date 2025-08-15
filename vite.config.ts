21a11332b992b1d5f94f9abfcd6032d3d2a5c19f
Alex Grin <mr.algrin@gmail.com>
2025-08-15 21:24:53 +0300

    configured storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   build: {
     copyPublicDir: false,
@@ -11,16 +18,42 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
       formats: ['es', 'cjs'],
-      fileName: (format, entryName) => `${entryName}.${format}.js`,
+      fileName: (format, entryName) => `${entryName}.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React',
+          react: 'React'
         }
       }
     }
   },
-  plugins: [react(), dts({rollupTypes: true, tsconfigPath: './tsconfig.app.json'})],
-})
+  plugins: [react(), dts({
+    rollupTypes: true,
+    tsconfigPath: './tsconfig.app.json'
+  })],
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
