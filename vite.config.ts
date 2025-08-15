0ec94e0f71ea333e96cb8566413fdf12e846eb17
Alex Grin <mr.algrin@gmail.com>
2025-08-15 18:35:48 +0300

    unused sources were removed and basic build configuration was set up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    copyPublicDir: false,
+    outDir: './dist',
+    lib: {
+      entry: resolve(__dirname, './src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        }
+      }
+    }
+  },
+  plugins: [react(), dts({rollupTypes: true, tsconfigPath: './tsconfig.app.json'})],
 })
